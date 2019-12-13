45b11974dab5940ac62473c622f1a1d98935914c
Fábio A. M. Pereira <famp@operamail.com>
2019-12-13 11:26:58 -0300

    Added the option to render user defined components

--- preact.js
+++ preact.js
@@ -13,7 +13,9 @@ const Preact = {
   },
 
   render(element, container) {
-    const componentInstance = new PreactDOMComponent(element);
+    const wrapperElement = this.createElement(TopLevelWrapper, element);
+    const componentInstance = new PreactCompositeComponentWrapper(wrapperElement);
+    //const componentInstance = new PreactDOMComponent(element);
     return componentInstance.mountComponent(container);
   },
 
@@ -28,6 +30,27 @@ const Preact = {
   }
 }
 
+class PreactCompositeComponentWrapper {
+  constructor(element) {
+    this._currentElement = element;
+  }
+
+  mountComponent(container) {
+    const Component = this._currentElement.type;
+    const componentInstance = new Component(this._currentElement.props);
+
+    let element = componentInstance.render();
+
+    while(typeof element.type === "function") {
+      element = (new element.type(element.props)).render();
+    }
+
+    const domComponentInstance = new PreactDOMComponent(element);
+    return domComponentInstance.mountComponent(container);
+  }
+}
+
+// ---------------------------- DOM Component -----------------------------------
 class PreactDOMComponent {
   constructor(element) {
     this._currentElement = element;
@@ -46,7 +69,47 @@ class PreactDOMComponent {
 
 }
 
+// ---------------------------- Top Level Wrapper -----------------------------------
+const TopLevelWrapper = function(props) {
+  this.props = props;
+};
+
+TopLevelWrapper.prototype.render = function() {
+  return this.props;
+};
+
+
+
+// Tests
+const MyTitle = Preact.createClass({
+  render() {
+    return Preact.createElement('h1', null, this.props.message);
+  }
+});
+
+const MyMessage = Preact.createClass({
+  render() {
+    if (this.props.asTitle) {
+      return (Preact.createElement(MyTitle, { message: this.props.message }));
+    }
+    else {
+      return Preact.createElement('p', null, this.props.message);
+    }
+  }
+})
+
 Preact.render(
-  Preact.createElement('h1', null, 'Hello World!'),
-  document.getElementById('root')
+  Preact.createElement(MyMessage, { asTitle: true, message: 'this is an h1 message' }),
+  document.getElementById('root1')
 );
+
+Preact.render(
+  Preact.createElement(MyMessage, { asTitle: false, message: 'and this is just a paragraph' }),
+  document.getElementById('root2')
+);
+
+Preact.render(
+  Preact.createElement('button', null, 'i\'m a primitive element'),
+  document.getElementById('root3')
+);
+
